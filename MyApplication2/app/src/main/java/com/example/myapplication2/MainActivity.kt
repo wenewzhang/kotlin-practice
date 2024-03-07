0e79dc54cddd5229e279d205c4e9ff8e04042a6e
Wenew Zhang <wenewboy@gmail.com>
2024-03-07 02:20:29 -0500

    Update MainActivity.kt
    kotlin course 3
    paint picture

--- MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -13,6 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import com.example.myapplication2.ui.theme.MyApplication2Theme
 import androidx.compose.ui.unit.dp
 
@@ -38,6 +46,17 @@ data class Message(val name:String, val title:String)
 @Composable
 fun Greeting(msg: Message, modifier: Modifier = Modifier) {
     Row(modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
         Column {
             Text(
                 text = "hello, ${msg.name}",
